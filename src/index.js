7e5103e9c6bf8e383b97a4ba6c9fe9f820ac66b8
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2023-08-27 00:11:54 +0100

    Read all files

--- src/index.js
+++ src/index.js
@@ -9,36 +9,44 @@ const openai = new OpenAI({
   apiKey: process.env.API_KEY,
 });
 
-// Initialize the parser
 const parser = csv.parse({
   delimiter: ",",
 });
-// Use the readable stream api to consume records
+
 parser.on("readable", async function () {
   let record;
   while ((record = parser.read()) !== null) {
     const review = await generateReview(record);
+    console.log(review);
     writeStream.write(review + os.EOL);
   }
 });
-// Catch any error
+
 parser.on("error", function (err) {
   console.error(err.message);
 });
-// Test that the parsed records matched the expected records
+
 parser.on("end", function () {
   console.log("Stream ended");
   writeStream.end();
 });
+
 const writeStream = fs.createWriteStream("./test/output.csv");
 writeStream.write(headers);
-// Write data to the stream
-parser.write(fs.readFileSync("./test/input-csvs/maryland-cookies.csv"));
-// Close the readable stream
+
+const files = fs.readdirSync("./test/input-csvs/");
+for (let file of files) {
+  if (file.includes(".csv")) {
+    // Write data to the stream
+    parser.write(fs.readFileSync("./test/input-csvs/" + file));
+  }
+}
+
 parser.end();
 
 async function generateReview(record) {
   const review = "fake review";
+
   // const completion = await openai.chat.completions.create({
   //   messages: [{ role: "user", content: "Say this is a test" }],
   //   model: "gpt-3.5-turbo",
